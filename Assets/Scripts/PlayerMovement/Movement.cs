09a416a47221f95147c49e4d548cca20acb97b66
CorneTm <termeercorne@gmail.com>
2024-03-18 13:54:08 +0100

    vhjbjnkjkhjgjfgyhjkbnv

--- Assets/Scripts/PlayerMovement/Movement.cs
+++ Assets/Scripts/PlayerMovement/Movement.cs
@@ -6,6 +6,10 @@ public class Movement : MonoBehaviour
     public float hor;
     public Vector3 lopen;
     public float speed;
+    public float crouchSpeedMultiplier = 0.5f; // Multiplier for crouch speed
+    public bool isCrouching = false; // Flag to track crouch state
+    public float standingHeight = 1.0f; // Original height of the player
+    public float crouchingHeight = 0.5f; // Height of the player when crouching
 
     // Start is called before the first frame update
     void Start()
@@ -17,21 +21,33 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Check for crouch input
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            isCrouching = true;
+            // Adjust player height when crouching
+            transform.localScale = new Vector3(transform.localScale.x, crouchingHeight, transform.localScale.z);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            isCrouching = false;
+            // Restore original player height when standing up
+            transform.localScale = new Vector3(transform.localScale.x, standingHeight, transform.localScale.z);
+        }
+
+        // Handle movement
         hor = Input.GetAxis("Horizontal");
         vert = Input.GetAxis("Vertical");
+
+        // Apply crouching speed multiplier if crouching
+        float currentSpeed = isCrouching ? speed * crouchSpeedMultiplier : speed;
+
         lopen.x = hor;
         lopen.z = vert;
 
-        transform.Translate(lopen * speed * Time.deltaTime);
+        transform.Translate(lopen * currentSpeed * Time.deltaTime);
     }
 
-    private void OnCollisionEnter(Collision infoAboutWhatIHit)
-    {
-        if (infoAboutWhatIHit.gameObject.tag == "MovingBlock")
-        {
-            print("Boem");
-        }
-    }
 
     // Method to set the player GameObject reference
     private void SetPlayerObjectReference()
