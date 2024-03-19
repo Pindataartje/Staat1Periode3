01d175d8acc8ed3cd7e533ed7b946b6f5d1593ec
Pindataartje <xbruintjes@gmail.com>
2024-03-19 15:56:14 +0100

    sdgDg

--- Assets/Scripts/Items/RandomLightScript.cs
+++ Assets/Scripts/Items/RandomLightScript.cs
@@ -3,22 +3,53 @@ using System.Collections;
 
 public class RandomLightScript : MonoBehaviour
 {
-    public GameObject lightObject; // Drag your light game object here in the inspector
+    public Light[] lights; // Lights controlled by this script
+    public GameObject enemy; // Enemy with the trigger collider
+
+    private bool playerInEnemyRange = false;
 
     void Start()
     {
-        StartCoroutine(RandomLight());
+        StartCoroutine(RandomLightToggle());
     }
 
-    IEnumerator RandomLight()
+    IEnumerator RandomLightToggle()
     {
         while (true)
         {
-            float delay = Random.Range(0f, 1.5f); // Generate a random delay between 0 and 5 seconds
-            yield return new WaitForSeconds(delay);
+            if (playerInEnemyRange)
+            {
+                // Toggle lights only if player is in range of the enemy
+                ToggleLights();
+            }
 
-            // Toggle the light on and off
-            lightObject.SetActive(!lightObject.activeSelf);
+            yield return new WaitForSeconds(Random.Range(0f, 1.5f)); // Random delay between light toggles
+        }
+    }
+
+    void ToggleLights()
+    {
+        foreach (Light light in lights)
+        {
+            light.enabled = !light.enabled;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == enemy)
+        {
+            // Player entered the enemy's trigger collider
+            playerInEnemyRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == enemy)
+        {
+            // Player exited the enemy's trigger collider
+            playerInEnemyRange = false;
         }
     }
 }
