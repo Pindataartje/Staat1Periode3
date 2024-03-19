10ef24ec45bc83290676f7f74596ee212f89b738
Pindataartje <xbruintjes@gmail.com>
2024-03-19 12:43:13 +0100

    wegg

--- Assets/Scripts/SaveLoadSystem/DataManager.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DataManager : MonoBehaviour
-{
-    public static DataManager instance { get; private set; }
-}
