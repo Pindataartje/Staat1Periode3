efb5d52c96fb5dcda485d6d6bc897189da96fffe
CorneTm <termeercorne@gmail.com>
2024-04-11 14:28:00 +0200

    asad

--- Assets/Scripts/PlayerMovement/Movement.cs
+++ Assets/Scripts/PlayerMovement/Movement.cs
@@ -6,13 +6,10 @@ public class Movement : MonoBehaviour
     public float hor;
     public Vector3 lopen;
     public float speed;
-    public float runSpeedMultiplier = 2.0f; // Multiplier for run speed
-    public bool isRunning = false; // Flag to track run state
     public float standingHeight = 1.0f; // Original height of the player
     public float crouchingHeight = 0.5f; // Height of the player when crouching
     public float maxRunDuration = 10f; // Maximum duration of running in seconds
-    private float currentRunDuration = 0f; // Current duration of running
-    
+   
 
     // Start is called before the first frame update
     void Start()
@@ -22,20 +19,7 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Check for run input and sufficient duration
-        if (Input.GetKeyDown(KeyCode.LeftShift) && currentRunDuration < maxRunDuration)
-        {
-            isRunning = true;
-        }
-        else if (Input.GetKeyUp(KeyCode.LeftShift) || currentRunDuration >= maxRunDuration)
-        {
-            isRunning = false;
-        }
-
-        // Update current run duration
-        currentRunDuration += isRunning ? Time.deltaTime : 0f;
-        currentRunDuration = Mathf.Clamp(currentRunDuration, 0f, maxRunDuration);
-
+     
         // Handle movement
         hor = Input.GetAxis("Horizontal");
         vert = Input.GetAxis("Vertical");
